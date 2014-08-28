2db9dc74c09b05a97453acd1857bc2a1fe0b51ce
Edvins Antonovs <ummahusla@gmail.com>
2014-08-28 16:18:45 +0100

    Create Parsing XML in easy way.php

---
+++ PHP/Parsing XML in easy way.php
@@ -0,0 +1,26 @@
+//requires simplexml http://us2.php.net/simplexml
+//this is a sample xml string
+$xml_string="<?xml version='1.0'?>
+<moleculedb>
+    <molecule name='Benzine'>
+        <symbol>ben</symbol>
+        <code>A</code>
+    </molecule>
+    <molecule name='Water'>
+        <symbol>h2o</symbol>
+        <code>K</code>
+    </molecule>
+</moleculedb>";
+ 
+//load the xml string using simplexml function
+$xml = simplexml_load_string($xml_string);
+ 
+//loop through the each node of molecule
+foreach ($xml->molecule as $record)
+{
+   //attribute are accessted by
+   echo $record['name'], '  ';
+   //node are accessted by -> operator
+   echo $record->symbol, '  ';
+   echo $record->code, '<br />';
+}
