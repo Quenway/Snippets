e56300083b8dc076087c060a84679690e9e65c0d
Edvins Antonovs <ummahusla@gmail.com>
2014-08-29 09:53:12 +0100

    Create Embedding YouTube Videos from Links.js

---
+++ JS/Embedding YouTube Videos from Links.js
@@ -0,0 +1,23 @@
+/*
+Useful script to create embedded Youtube videos from links with custom parameters.
+It works with mostly every Youtube link format since it uses a pretty forgiving regex,
+but it may not work for every single case. This script also fixes the tedious "super z-index"
+issue that you may have experienced when embedding Youtube videos.
+*/
+
+function embedYoutube(link, ops) {
+ 
+  var o = $.extend({
+    width: 480,
+    height: 320,
+    params: ''
+  }, ops);
+ 
+  var id = /\?v\=(\w+)/.exec(link)[1];
+ 
+  return '<iframe style="display: block;"'+
+    ' class="youtube-video" type="text/html"'+
+    ' width="' + o.width + '" height="' + o.height +
+    ' "src="http://www.youtube.com/embed/' + id + '?' + o.params +
+    '&amp;wmode=transparent" frameborder="0" />';
+}
